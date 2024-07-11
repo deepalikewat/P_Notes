843df607dfa9c6fbfe692d88c905e9a5f2d41afa
deepalikewat <121901064+deepalikewat@users.noreply.github.com>
2024-07-11 12:15:53 +0530

    Update mess.js
    un10

--- module/mess.js
+++ module/mess.js
@@ -7,5 +7,3 @@ const Me = new mongoose.Schema({
 
 })
 module.exports=mongoose.models.mess || mongoose.model("mess",Me)
-//agar mon name ka koi module hai to usko le lo agar nai hai to create kar do
-//agar existing db me change hoga to new server run karo
